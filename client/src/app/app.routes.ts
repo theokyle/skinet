396eaa371d6bb7dfb0d2c74c28af8321592d8ef8
theokyle <theokyle@gmail.com>
2025-12-09 23:48:51 -0600

    updated cart components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { ProductDetails } from './features/shop/product-details/product-details'
 import { TestError } from './features/test-error/test-error';
 import { NotFound } from './shared/components/not-found/not-found';
 import { ServerError } from './shared/components/server-error/server-error';
+import { Cart } from './features/cart/cart';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: "shop", component: Shop},
     {path: "shop/:id", component: ProductDetails},
+    {path: "cart", component: Cart},
     {path: "test-error", component: TestError},
     {path: "not-found", component: NotFound},
     {path: "server-error", component: ServerError},
